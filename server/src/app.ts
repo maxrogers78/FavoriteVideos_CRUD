e0c81f57924fbfe9693d3ec7d366787a3b8ac481
Max <maxrogerssepulveda@gmail.com>
2020-10-24 16:59:11 -0300

    basic morgan, express and cors config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,21 @@
+// Express
 import express from "express";
 import config from "./config";
 const app = express();
+// cors
+import cors from "cors";
+// Morgan
+import morgan from "morgan";
+// Routes
+import videosRoutes from "./routes/videos.routes";
 
 app.set("port", config.PORT);
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(videosRoutes);
 
 export default app;
